7bbd6b2ea0967e770ab2b7fa7b7af7b68ce6b0f1
martinjohn27 <martinjohn24517@gmail.com>
2020-09-25 00:13:25 +0800

    Basic components and Angular material installed

---
+++ BIT306/CTS/src/app/CTS-Login/login.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, ValidationErrors } from '@angular/forms';
+
+@Component({
+  selector: 'app-cts-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class CtsLoginComponent {
+
+}
--- BIT306/CTS/src/app/app.module.ts
+++ BIT306/CTS/src/app/app.module.ts
@@ -3,12 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { CtsLoginComponent } from './CTS-Login/login.component';
+import { CtsSignupComponent } from './CTS-Signup/signup.component';
+import { HomeComponent } from './Home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+//Angular Material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CtsLoginComponent,
+    CtsSignupComponent,
+    HomeComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
