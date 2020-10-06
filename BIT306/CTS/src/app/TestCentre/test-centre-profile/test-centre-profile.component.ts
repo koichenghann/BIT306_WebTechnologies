bec498b54841f846bd390c0e99b6ff490b6936c2
ChengHannKoi <chengghannkoi@gmail.com>
2020-10-06 09:39:23 +0800

    update

--- BIT306/CTS/src/app/TestCentre/test-centre-profile/test-centre-profile.component.ts
+++ BIT306/CTS/src/app/TestCentre/test-centre-profile/test-centre-profile.component.ts
@@ -1,4 +1,7 @@
+import { Input } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { TestCentre } from '../test-centre.model';
+import { TestCentreService } from '../test-centre.service';
 
 @Component({
   selector: 'app-test-centre-profile',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestCentreProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(public testCentreService: TestCentreService) { }
 
   ngOnInit(): void {
+    this.testCentres = this.testCentreService.getTestCentres();
   }
 
+  @Input() testCentres: TestCentre[] = [];
+
+  currentOfficer = 'Norhisshan';
+
+  tcid = this.testCentreService.generateNewId();
+  tcofficer = this.currentOfficer;
+  tcstate = null;
+  tcaddress = null;
+  tccontact = null;
+  tccapacity = null;
 }
